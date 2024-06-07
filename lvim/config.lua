f2a469f23ad9a2c2399787921547358f2523720c
iced-penguin <icedpenguin0504@gmail.com>
2024-06-08 02:03:21 +0900

    chore: LunarVimの設定を変更
    カラースキームとキーバインド

--- lvim/config.lua
+++ lvim/config.lua
@@ -3,3 +3,14 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+
+-- https://www.lunarvim.org/docs/configuration/plugins/user-plugins
+lvim.plugins = {
+  {
+    "gbprod/nord.nvim",
+  }
+}
+
+lvim.colorscheme = "nord"
+
+lvim.keys.insert_mode["jj"] = "<ESC>"
